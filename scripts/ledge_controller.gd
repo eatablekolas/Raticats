73d24eea088a969037a9c9dcfe0ac8d73ca404dc
eatablekolas <tymek2004@gmail.com>
2024-02-12 14:29:34 +0100

    Refactored the ledge controller code
    Now it's possible to add more ledge colliders to the player, without the need to change the code every time.

--- scripts/ledge_controller.gd
+++ scripts/ledge_controller.gd
@@ -2,21 +2,28 @@ extends Node3D
 
 @export var test_label: Label
 
-@export var high_ledge: Area3D
-@export var medium_ledge: Area3D
-@export var low_ledge: Area3D
+var ledge_colliders: Dictionary = {}
 
-# I assume that since the collider only detects bodies on the 'obstacle' layer,
-# there's no need for additional checks
+func _ready() -> void:
+	for collider: Area3D in get_children():
+		if !collider.name.is_valid_int():
+			push_warning("One of the player's ledge colliders doesn't have a valid integer in its' name!")
+		
+		# The key of a collider in the dictionary is also its' height
+		var height: int = collider.name.to_int()
+		ledge_colliders[height] = collider
+
 func get_ledge_height() -> int:
-	if high_ledge.has_overlapping_areas():
-		return 3
-	elif medium_ledge.has_overlapping_areas():
-		return 2
-	elif low_ledge.has_overlapping_areas():
-		return 1
-	else:
-		return 0
+	var max_height: int = 0
+	
+	# For some reason, in GDScript, a for loop goes through keys in a dictionary, not values
+	for height: int in ledge_colliders:
+		if height > max_height && ledge_colliders[height].has_overlapping_areas():
+			# I assume that since the collider only detects areas on the 'ledge' layer,
+			# there's no need for additional checks
+			max_height = height
+	
+	return max_height
 
 func _process(_delta) -> void:
 	test_label.text = str(get_ledge_height())
