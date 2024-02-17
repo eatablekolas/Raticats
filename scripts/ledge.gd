074003ff5ab6dfe1eec0d96792efa99d920078f0
eatablekolas <tymek2004@gmail.com>
2024-02-17 14:28:23 +0100

    Created Ledge class

---
+++ scripts/ledge.gd
@@ -0,0 +1,6 @@
+class_name Ledge
+
+var exists: bool = false
+var level: int = 0
+var position: Vector3 = Vector3.ZERO
+var rotation: float = 0.0
--- scripts/ledge_controller.gd
+++ scripts/ledge_controller.gd
@@ -6,6 +6,8 @@ class_name LedgeController
 @export var height_ray: RayCast3D
 @export var test_label: Label
 
+var current_level = 0
+
 # Checks if space above the ledge is free
 func is_space_above_free() -> bool:
 	for ray: RayCast3D in above_rays.get_children():
@@ -39,10 +41,30 @@ func get_ledge_level() -> int:
 		if level > max_level and is_hitting_on_level(ray_level.name):
 			max_level = level
 	
+	current_level = max_level
 	return max_level
 
-func get_ledge_height():
-	print("Collision point: %s\nCollision normal: %s" % [height_ray.get_collision_point(), height_ray.get_collision_normal()])
+func get_ledge_height() -> float:
+	return height_ray.get_collision_point().y
+
+# WORK IN PROGRESS
+func get_ledge_position() -> Vector3:
+	return Vector3.ZERO
+
+func get_ledge_rotation() -> float:
+	return 0.0
+# WORK IN PROGRESS
+
+func get_ledge() -> Ledge:
+	var ledge = Ledge.new()
+	ledge.level = get_ledge_level()
+	
+	if ledge.level == 0:
+		return ledge
+	
+	ledge.position = get_ledge_position()
+	ledge.rotation = get_ledge_rotation()
+	return ledge
 
 # For testing purposes - should remove later
 func _process(_delta):
